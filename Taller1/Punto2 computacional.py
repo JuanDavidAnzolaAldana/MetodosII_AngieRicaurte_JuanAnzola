2ea3c902003253fb764a02fb90e1fe48d65ec998
JuanDavidAnzolaAldana <j.anzolaa@uniandes.edu.co>
2024-01-29 20:16:05 -0500

    Punto 2 computacional terminado

--- Taller1/Punto2 computacional.py
+++ Taller1/Punto2 computacional.py
@@ -3,8 +3,6 @@ import numpy
 import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import tqdm
-import time
-import random
 k=1
 def dot(a:numpy.ndarray,b:numpy.ndarray)->float:
     if a.shape!=b.shape:
@@ -33,6 +31,7 @@ class particle:
         state[2]=self.a
         state[3]=self.mass*self.vel
         state[4,0]=self.u
+        state[4,1]=((self.pos[0]*self.vel[1])-(self.pos[1]*self.vel[0]))*self.mass
         return state
     def collide(self,other)->None:
         d=other.r+self.r-norm(other.pos-self.pos)
@@ -98,70 +97,77 @@ def simulation(tf:float,dt:float,fr:int)->None:
                particle(numpy.random.uniform(-19,19,size=2), numpy.random.uniform(-5,5,size=2), 1.0, 1.0, "pink"),
                particle(numpy.random.uniform(-19,19,size=2), numpy.random.uniform(-5,5,size=2), 1.0, 1.0, "gray")]
     info,tl=simulate(particles,box,tf,dt)
-    fig=plt.figure(figsize=(12,8))
-    ax=fig.add_subplot(231)
-    ax1=fig.add_subplot(232)
-    ax2=fig.add_subplot(233)
-    ax3=fig.add_subplot(234)
-    ax4=fig.add_subplot(235)
+    fig=plt.figure(figsize=(16,8))
+    ax=fig.add_subplot(241)
+    ax1=fig.add_subplot(242)
+    ax2=fig.add_subplot(243)
+    ax3=fig.add_subplot(245)
+    ax4=fig.add_subplot(246)
+    ax5=fig.add_subplot(247)
+    ax6=fig.add_subplot(244)
     def start():
-        ax.set(xlim=box.get_limits_x(),ylim=box.get_limits_y())
-    def update(i):
-        ax.clear()
         ax1.clear()
         ax2.clear()
         ax3.clear()
         ax4.clear()
-        start()
-        ax.set_title(r'$ t=%.2f \ s$' %(tl[i*fr]))
+        ax5.clear()
+        ax6.clear()
         ax1.set_title("Momento lineal en x")
         ax2.set_title("Momento lineal en y")
         ax3.set_title("Energía cinética")
         ax4.set_title("Energía potencial")
+        ax5.set_title("Energía mecánica")
+        ax6.set_title("Momento angular")
         x1=numpy.zeros_like(tl)
         x2=numpy.zeros_like(tl)
         x3=numpy.zeros_like(tl)
         x4=numpy.zeros_like(tl)
+        x6=numpy.zeros_like(tl)
+        for j in range(len(info)):
+            kinetic=numpy.sum(info[j][:,3]*info[j][:,1],axis=1)/2
+            ax1.plot(tl,info[j][:,3,0],c=particles[j].color)
+            ax2.plot(tl,info[j][:,3,1],c=particles[j].color)
+            ax3.plot(tl,kinetic,c=particles[j].color)
+            ax4.plot(tl,info[j][:,4,0],c=particles[j].color)
+            ax5.plot(tl,info[j][:,4,0]+kinetic,c=particles[j].color)
+            ax6.plot(tl,info[j][:,4,1],c=particles[j].color)
+            x1+=info[j][:,3,0]
+            x2+=info[j][:,3,1]
+            x3+=kinetic
+            x4+=info[j][:,4,0]
+            x6+=info[j][:,4,1]
+        ax1.plot(tl,x1,linestyle="--",c="red",label="total")
+        ax2.plot(tl,x2,linestyle="--",c="red",label="total")
+        ax3.plot(tl,x3,linestyle="--",c="red",label="total")
+        ax4.plot(tl,x4/2,linestyle="--",c="red",label="total")
+        ax5.plot(tl,x4/2+x3,linestyle="--",c="red",label="total")
+        ax6.plot(tl,x6,linestyle="--",c="red",label="total")
+        ax1.legend()
+        ax2.legend()
+        ax3.legend()
+        ax4.legend()
+        ax5.legend()
+        ax6.legend()
+    def update(i):
+        ax.clear()
+        ax.set(xlim=box.get_limits_x(),ylim=box.get_limits_y())
+        ax1.set(xlim=(0,tl[i*fr]))
+        ax2.set(xlim=(0,tl[i*fr]))
+        ax3.set(xlim=(0,tl[i*fr]))
+        ax4.set(xlim=(0,tl[i*fr]))
+        ax5.set(xlim=(0,tl[i*fr]))
+        ax6.set(xlim=(0,tl[i*fr]))
+        ax.set_title(r'$ t=%.2f \ s$' %(tl[i*fr]))
         for j in range(len(info)):
             kinetic=numpy.sum(info[j][:,3]*info[j][:,1],axis=1)/2
             st=info[j][i*fr]
             ax.add_patch(plt.Circle((st[0,0],st[0,1]),particles[j].r, fill=True, color=particles[j].color))
             ax.arrow(st[0,0],st[0,1],st[1,0],st[1,1],color='r',head_width=1,length_includes_head=True)
             ax.arrow(st[0,0],st[0,1],st[2,0],st[2,1],color='b',head_width=1,length_includes_head=True)
-            ax1.plot(tl,info[j][:,3,0],c=particles[j].color)
-            ax2.plot(tl,info[j][:,3,1],c=particles[j].color)
-            ax3.plot(tl,kinetic,c=particles[j].color)
-            ax4.plot(tl,info[j][:,4,0],c=particles[j].color)
-            x1+=info[j][:,3,0]
-            x2+=info[j][:,3,1]
-            x3+=kinetic
-            x4+=info[j][:,4,0]
-        ax1.plot(tl,x1,linestyle="--",c="red",label="total")
-        ax2.plot(tl,x2,linestyle="--",c="red",label="total")
-        ax3.plot(tl,x3,linestyle="--",c="red",label="total")
-        ax4.plot(tl,x4,linestyle="--",c="red",label="total")
-        ax1.legend()
-        ax2.legend()
-        ax3.legend()
-        ax4.legend()
-        ax1.axvline(tl[i*fr],c="r")
-        ax2.axvline(tl[i*fr],c="r")
-        ax3.axvline(tl[i*fr],c="r")
-        ax4.axvline(tl[i*fr],c="r")
-    start()
     Animation = anim.FuncAnimation(fig,update,frames=int(len(tl)/fr),init_func=start,interval=1000*dt*fr)
     plt.show()
-while True:
-    a=input("""\nProblema 2.08 Choques de duración finita\nSeleccione:
-1. Correr simulación y mostrar la gráficas de momento y energía.\n2. ¿Qué significa la constate K?\n3. Salir.\n""")
-    if a=="1":
-        k=float(input("Ingrese la constante K.\n"))
-        simulation(float(10), float(0.001),50)
-    elif a=="2":
-        print("Creo que el significado de la constante K es la resistencia a la deformación del material. Si un material tiene una K pequeña, este se deformará fácilmente y será más fácil atravesar el material en lugar de colisionar con el. Si K es un número grande, el material no se deformará y será más fácil chocar con él que atravesarlo.")
-    elif a=="3":
-        print("adios")
-        break
-    else:
-        print("opción inválida")
+print("Se va a correr la simulación y se va a mostrar todas las gráficas de momento lineal, momento angular y energía al mismo tiempo.")
+print("las respuestas a las preguntas se encuentran en un archivo llamado \"punto 2 teórico\".")
+k=float(input("Ingrese la constante K.\n"))
+simulation(float(10), float(0.001),200)
 
